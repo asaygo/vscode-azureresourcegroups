70bed3ce2a8d9d0866f74ce8689e739d87cddf15
Alex Weininger <alex.weininger@live.com>
2022-06-02 14:23:37 -0700

    Don't use childChildImpl to create rg

--- src/commands/createResourceGroup.ts
+++ src/commands/createResourceGroup.ts
@@ -3,11 +3,12 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { IActionContext } from '@microsoft/vscode-azext-utils';
+import { IResourceGroupWizardContext, LocationListStep, ResourceGroupCreateStep, ResourceGroupNameStep } from '@microsoft/vscode-azext-azureutils';
+import { AzureWizard, AzureWizardExecuteStep, AzureWizardPromptStep, ExecuteActivityContext, IActionContext, nonNullProp } from '@microsoft/vscode-azext-utils';
 import { window } from 'vscode';
 import { ext } from '../extensionVariables';
-import { ResourceGroupTreeItem } from '../tree/ResourceGroupTreeItem';
 import { SubscriptionTreeItem } from '../tree/SubscriptionTreeItem';
+import { createActivityContext } from '../utils/activityUtils';
 import { localize } from '../utils/localize';
 
 export async function createResourceGroup(context: IActionContext, node?: SubscriptionTreeItem): Promise<void> {
@@ -15,7 +16,22 @@ export async function createResourceGroup(context: IActionContext, node?: Subscr
         node = await ext.appResourceTree.showTreeItemPicker<SubscriptionTreeItem>(SubscriptionTreeItem.contextValue, context);
     }
 
-    const rgNode: ResourceGroupTreeItem = await node.createChild(context);
+    const wizardContext: IResourceGroupWizardContext & ExecuteActivityContext = {
+        ...context, ...node.subscription, suppress403Handling: true,
+        ...(await createActivityContext()),
+    };
 
-    void window.showInformationMessage(localize('createdRg', 'Created resource group "{0}".', rgNode.name));
+    const title: string = localize('createResourceGroup', 'Create Resource Group');
+    const promptSteps: AzureWizardPromptStep<IResourceGroupWizardContext>[] = [new ResourceGroupNameStep()];
+    LocationListStep.addStep(wizardContext, promptSteps);
+    const executeSteps: AzureWizardExecuteStep<IResourceGroupWizardContext>[] = [new ResourceGroupCreateStep()];
+    const wizard: AzureWizard<IResourceGroupWizardContext & ExecuteActivityContext> = new AzureWizard(wizardContext, { title, promptSteps, executeSteps });
+    await wizard.prompt();
+    const newResourceGroupName = nonNullProp(wizardContext, 'newResourceGroupName');
+    wizardContext.activityTitle = localize('createResourceGroup', 'Create Resource Group "{0}"', newResourceGroupName);
+    await wizard.execute();
+
+    await ext.appResourceTree.refresh(context);
+
+    void window.showInformationMessage(localize('createdRg', 'Created resource group "{0}".', newResourceGroupName));
 }
